33adcfbe0e19593ca250568c3e8ec527cc961e67
Francisco Pérez <fsistemas@gmail.com>
2022-09-17 19:03:55 -0500

    Easy to new users with good defaults: - Handle -output file -format json and save result to file.json - Allow user to test the tool without config file: If user does not provide -config config.json then try to find a config.json in USER_HOME/.gosql2json/config.json and fallback to a default to a sqlite databasse(test.db) in current folder

--- core/init.go
+++ core/init.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"strings"
 
@@ -10,36 +9,47 @@ import (
 )
 
 type Queries map[string]string
-type Conections map[string]string
+type Connections map[string]string
 
 type ConfigFile struct {
-	Conections Conections `json:"conections"`
-	Queries    Queries    `json:"queries"`
+	Connections Connections `json:"connections"`
+	Queries     Queries     `json:"queries"`
 }
 
 type RunConfig struct {
-	ConfigFile    string
-	ConectionName string
-	QueryName     string
-	Wrapper       string
-	FirstOnly     bool
-	KeyName       string
-	ValueName     string
-	Output        string
-	Format        string
+	ConfigFile     string
+	ConnectionName string
+	QueryName      string
+	Wrapper        string
+	FirstOnly      bool
+	KeyName        string
+	ValueName      string
+	Output         string
+	Format         string
 }
 
-func (config *ConfigFile) GetConection(conectionName string) string {
-	rawQuery := config.Conections[conectionName]
+func (config *RunConfig) GetOutputFileName() string {
+	fileName := config.Output
+	ext := "." + config.Format
+
+	if !strings.HasSuffix(strings.ToLower(fileName), ext) {
+		fileName = fileName + ext
+	}
+
+	return fileName
+}
+
+func (config *ConfigFile) GetConnection(connectionName string) string {
+	rawQuery := config.Connections[connectionName]
 
 	if rawQuery == "" {
-		return conectionName
+		return connectionName
 	}
 
 	return rawQuery
 }
 
-func (config *ConfigFile) GetQuery(queryName string) string {
+func (config *ConfigFile) GetQuery(queryName string) (string, error) {
 	rawQuery := config.Queries[queryName]
 
 	if rawQuery == "" {
@@ -50,20 +60,35 @@ func (config *ConfigFile) GetQuery(queryName string) string {
 		var err error
 		rawQuery, err = io.LoadFileContentAsString(rawQuery[1:])
 		if err != nil {
-			panic(err.Error())
+			return "", err
 		}
 	}
 
-	return rawQuery
+	return rawQuery, nil
 }
 
 func NewConfigFile(file string) (*ConfigFile, error) {
+	configFileName := file
 	config := new(ConfigFile)
+
 	if file == "" {
-		return nil, fmt.Errorf("file is required")
+		configFileName = io.GetEnvOrDefault("HOME", ".") + "/.gosql2json/config.json"
+
+		if !io.FileExists(configFileName) {
+			//Fallback to allow user to test the tool without any configuration
+			config.Connections = Connections{
+				"default": "sqlite+test.db",
+			}
+
+			config.Queries = Queries{
+				"default": "SELECT 1 AS a, 2 AS b",
+			}
+
+			return config, nil
+		}
 	}
 
-	f, err := os.Open(file)
+	f, err := os.Open(configFileName)
 	if err != nil {
 		return nil, err
 	}
@@ -77,7 +102,4 @@ func NewConfigFile(file string) (*ConfigFile, error) {
 	}
 
 	return config, nil
-}
-
-type GoSqlTo struct {
 }
---
+++ core/init_test.go
@@ -0,0 +1,117 @@
+package core
+
+import "testing"
+
+func TestRunConfig_GetOutputFileName(t *testing.T) {
+	runConfigWithoutExt := createRunConfig("my_output")
+
+	if "my_output.json" != runConfigWithoutExt.GetOutputFileName() {
+		t.Log("Expected my_output.json")
+		t.Fail()
+	}
+
+	runConfigWithExt := createRunConfig("my_output.json")
+
+	if "my_output.json" != runConfigWithExt.GetOutputFileName() {
+		t.Log("Expected my_output.json")
+		t.Fail()
+	}
+}
+
+func TestConfigFile_GetConnection(t *testing.T) {
+	configFile := createConfigFile()
+
+	connString := configFile.GetConnection("CONN_STRING")
+	if "CONN_STRING" != connString {
+		t.Log("Expected Connection: CONN_STRING")
+		t.Fail()
+	}
+
+	connString = configFile.GetConnection("CONN_STRING_EXIST")
+	if "ANY_STRING" != connString {
+		t.Log("Expected Connection: ANY_STRING")
+		t.Fail()
+	}
+}
+
+func TestConfigFile_GetQuery(t *testing.T) {
+	configFile := createConfigFile()
+
+	queryString, err := configFile.GetQuery("MY_QUERY")
+	if err != nil || "MY_QUERY" != queryString {
+		t.Log("Expected Query: MY_QUERY", err)
+		t.Fail()
+	}
+
+	queryString, err = configFile.GetQuery("QUERY_OK")
+	if err != nil || "SELECT 1 AS ok" != queryString {
+		t.Log("Expected Query: SELECT 1 AS ok", err)
+		t.Fail()
+	}
+
+	queryString, err = configFile.GetQuery("@../testdata/query.sql")
+	if err != nil || "SELECT 1 AS a, 2 AS b" != queryString {
+		t.Log("Expected Query: SELECT 1 AS a, 2 as b. But was: ", queryString, err)
+		t.Fail()
+	}
+
+	queryString, err = configFile.GetQuery("@../testdata/query_does_not_exist.sql")
+	if err == nil {
+		t.Log("Expected Query: nil with error because file query does not exist", queryString)
+		t.Fail()
+	}
+}
+
+func TestNewConfigFile(t *testing.T) {
+	configFile, err := NewConfigFile("../testdata/config.json")
+
+	if err != nil {
+		t.Log("Expected configFile without error. But was: ", configFile, err)
+		t.Fail()
+	}
+
+	configFile, err = NewConfigFile("")
+	if err != nil {
+		t.Log("Expected fallback configFile without error. But was: ", configFile, err)
+		t.Fail()
+	}
+
+	configFile, err = NewConfigFile("../testdata/config_does_not_exist.json")
+
+	if err == nil {
+		t.Log("Expected error, because ../testdata/config_does_not_exist.json does not exist. But got: ", configFile)
+		t.Fail()
+	}
+
+	configFile, err = NewConfigFile("../testdata/file_exists.txt")
+
+	if err == nil {
+		t.Log("Expected error, because ../testdata/file_exists.txt is not a valid config file. But got: ", configFile)
+		t.Fail()
+	}
+}
+
+func createRunConfig(output string) RunConfig {
+	return RunConfig{
+		ConfigFile:     "config.json",
+		Format:         "json",
+		ConnectionName: "default",
+		QueryName:      "default",
+		KeyName:        "",
+		ValueName:      "",
+		FirstOnly:      false,
+		Output:         output,
+		Wrapper:        "",
+	}
+}
+
+func createConfigFile() ConfigFile {
+	return ConfigFile{
+		Connections: Connections{
+			"CONN_STRING_EXIST": "ANY_STRING",
+		},
+		Queries: Queries{
+			"QUERY_OK": "SELECT 1 AS ok",
+		},
+	}
+}
